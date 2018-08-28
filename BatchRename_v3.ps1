dcf7dedcdc7cf095f2d1a3b4628d39e1e274e3db
hacronymus <mhoustle@gmail.com>
2018-08-28 14:57:25 -0400

    Initial commit
    Adding a README and the current working version of the script

---
+++ BatchRename_v3.ps1
@@ -0,0 +1,91 @@
+﻿$ListFile = Read-Host -Prompt "Enter Rename CSV File:"
+$destDir = Read-Host -Prompt "Enter Target Folder:"
+
+#$ListFile = "C:\Users\mhous\OneDrive\Scripts\RenameTest.csv"
+#$destDir = "C:\Users\mhous\OneDrive\Scripts\TestFolder"
+
+$ListFile = $ListFile -replace '"',''
+$destDir = $destDir -replace '"',''
+
+#write-host $ListFile
+#write-host $destDir
+
+
+cd $destDir
+
+$fileList = Get-ChildItem -Attributes !Directory -Recurse
+$List = Import-Csv $ListFile
+
+$missedFile = New-Object System.Collections.Generic.List[System.Object]
+$missingFile = New-Object System.Collections.Generic.List[System.Object]
+$existingFile = New-Object System.Collections.Generic.List[System.Object]
+
+write-host "---Existing Files---"
+
+foreach ($Listx in $List) {
+    $old = $listx.old
+    $new = $listx.new
+
+    $old = $old -replace '[(]','[(]'
+    $old = $old -replace '[)]','[)]'
+
+    #write-host $old
+
+    $file = Get-ChildItem "$old*" -Name -Recurse
+
+     $ErrorLast = $Error[0]
+    
+    Get-ChildItem "$old*" -Recurse | Rename-Item -NewName { $_.Name -Replace $old , $new}
+    If ($ErrorLast -ne $Error[0]) 
+            {
+        $ErrorWrite = "null"
+        $ErrorWrite = ("-----------------" , $(Get-Date) , $Error[0].Exception , $Error[0].TargetObject)
+        Write-host ("New Error" , $ErrorWrite)
+        ##Out-File -FilePath C:\Users\mhous\OneDrive\Shows\MeanGirls\Scripts\log.txt" $ErrorWrite
+        Out-File -FilePath log.txt -Append -InputObject $ErrorWrite
+        }
+   # Else {Write-host "Same Error, Ignore" $ErrorLast}
+    
+    
+    #Out-File -FilePath log.txt -Append -InputObject "************************************************************"
+
+    if ($file -ne $null) {
+
+        write-host $file -ForegroundColor Green
+        $existingFile.add($file)
+
+    } 
+    else {$missingFile.add($listx.old)} 
+    
+
+}
+
+foreach ($file in $fileList.Name) {
+
+    if (!($existingFile.contains($file))) {
+
+        $missedFile.add($file)
+
+    }
+
+}
+
+Write-Host "---Missed Files---"
+
+foreach ($file in $missedFile) {
+
+    write-host $file -ForegroundColor Red
+
+}
+
+Write-Host "---Missing Files---"
+
+foreach ($file in $missingFile) {
+
+    write-host $file -ForegroundColor magenta
+
+}
+
+#dir -Name
+
+Read-Host
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # batch-rename
+
+Powershell script that renames files in a folder and subfolders using a CSV file.
+
+-Script will ask for folder to work in when started.
+-Script will ask for CSV file to reference.
+	-CSV should have a 'NEW' and 'OLD' column
+-Script does a find and replace of strings in the file.
+-Script only matches beginning of string and will ignore differences at the end
+e.g:
+File_01_V01
+File_01_V02
+
+if the string in the 'OLD' column is "File_01" both files will be selected and renamed by what is in the 'NEW' column.
+
+*NOTE* Watch for 'OLD' names that are not unique enough!
+
+e.g.
+RENAME
+RENAME - Copy(1)
+RENAME - Copy(2)
+
+If "RENAME" and "RENAME - Copy(1)" are both in the 'OLD' column "RENAME" will rename all of the files and "RENAME - Copy(1)" will not find a matching file.
+
+-Script reports files that are in the folder but no in the 'OLD' list
+-Script reports files that are in the 'OLD' list but not in the folder.
